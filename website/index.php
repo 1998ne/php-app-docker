89fefb6c7d00fc31ddb34258fa70ab2b3dc0f5c4
1998ne <86507245+1998ne@users.noreply.github.com>
2025-07-04 12:33:27 +0530

    Update index.php

--- website/index.php
+++ website/index.php
@@ -4,3 +4,14 @@ require 'config.php';
 require 'functions.php';
 
 run();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP App</title>
+</head>
+<body>
+    <h1>Hello from Neha's CI/CD Docker Project 🚀</h1>
+</body>
+</html>
